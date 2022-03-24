42eb65860d75d3a3aedefbc5fd300830e11e48d3
Lukas Muzik <lukas59@email.cz>
2022-03-24 21:46:03 +0100

    location cmds

--- uun_meteoapp_maing01-server/app/api/controllers/location-controller.js
+++ uun_meteoapp_maing01-server/app/api/controllers/location-controller.js
@@ -3,6 +3,18 @@ const LocationAbl = require("../../abl/location-abl.js");
 
 class LocationController {
 
+  list(ucEnv) {
+    return LocationAbl.list(ucEnv.getUri().getAwid(), ucEnv.getDtoIn());
+  }
+
+  update(ucEnv) {
+    return LocationAbl.update(ucEnv.getUri().getAwid(), ucEnv.getDtoIn());
+  }
+
+  delete(ucEnv) {
+    return LocationAbl.delete(ucEnv.getUri().getAwid(), ucEnv.getDtoIn());
+  }
+
   get(ucEnv) {
     return LocationAbl.get(ucEnv.getUri().getAwid(), ucEnv.getDtoIn());
   }
--- uun_meteoapp_maing01-server/app/api/validation_types/location-types.js
+++ uun_meteoapp_maing01-server/app/api/validation_types/location-types.js
@@ -1,9 +1,23 @@
 /* eslint-disable */
 const locationCreateDtoInType = shape({
-    locationCode: string(200).isRequired(),
-    name: string(200).isRequired(),
-  })
-  
-const locationGetDtoInType = shape({
-    locationCode: string(200).isRequired()
+  name: string(200).isRequired(),
 })
+const locationGetDtoInType = shape({
+  locationCode: string(200).isRequired()
+})
+const locationDeleteDtoInType = shape({
+  locationCode: string(200).isRequired()
+})
+const locationUpdateDtoInType = shape({
+  locationCode: string(200).isRequired(),
+  name: string(200).isRequired()
+})
+const locationListDtoInType = shape({
+  sortBy: oneOf(["name"]),
+  order: oneOf(["asc", "desc"]),
+  state: oneOf(["active", "passive"]),
+  pageInfo: shape({
+    pageIndex: integer(),
+    pageSize: integer(),
+  }),
+});
